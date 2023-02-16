5c9d4f1aef925e4590dbbdced8d95880ab587a2c
hanjae0205 <72303089+hanjae0205@users.noreply.github.com>
2023-02-16 21:15:27 +0900

    save server in Github

--- server.bat
+++ server.bat
@@ -1,7 +1,7 @@
 @echo off
+git remote add origin "github.com/hanjae0205/server.git"
 java -Xmx1024M -Xms1024M -jar server.jar nogui
 git init
 git add .
 git commit -m "save server in Github"
-git remote add origin "github.com/hanjae0205/seuver.git"
 git push -u origin main
