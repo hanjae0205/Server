3515f471240775f1e6a38edfb7001dc9f30779c3
hanjae0205 <72303089+hanjae0205@users.noreply.github.com>
2023-02-22 19:50:10 +0900

    message

--- server.bat
+++ server.bat
@@ -1,5 +1,5 @@
 @echo off
-git clone "https://github.com/hanjae0205/server.git"
+git pull "https://github.com/hanjae0205/server.git"
 git commit -m "Start server"
 java -Xmx1024M -Xms1024M -jar server.jar nogui
 git init
