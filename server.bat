3e62ff445dab4c5fcf52d070f4d4117ff14a878f
hanjae0205 <72303089+hanjae0205@users.noreply.github.com>
2023-02-28 00:47:08 +0900

    save server in Github

--- server.bat
+++ server.bat
@@ -1,6 +1,10 @@
 @echo off
 git pull "https://github.com/hanjae0205/server.git"
+echo "server started">./serverstart.txt
+git init
+git add
 git commit -m "Start server"
+git push -u origin main
 java -Xmx1024M -Xms1024M -jar server.jar nogui
 git init
 git add .
