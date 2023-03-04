b80c88ab52c71174f9fb199b429b449483b271c7
hanjae0205 <72303089+hanjae0205@users.noreply.github.com>
2023-03-04 16:02:27 +0900

    @everyone Stop&Save server in Github

--- server.bat
+++ server.bat
@@ -3,11 +3,11 @@ git pull "https://github.com/hanjae0205/server.git"
 echo "server started">./serverstart.txt
 git init
 git add .
-git commit -m "Start server"
+git commit -m "@everyone Start server"
 git push -u origin main
 java -Xmx2G -Xms512M -jar server.jar nogui
 del serverstart.txt
 git init
 git add .
-git commit -m "Stop&Save server in Github"
+git commit -m "@everyone Stop&Save server in Github"
 git push -u origin main
